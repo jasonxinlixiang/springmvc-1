6b0d949df09f9a6691eacf3d96787068f7e3ead0
向心力 <>
2020-02-27 15:58:23 +0800

    Create entities and daos classes

---
+++ src/com/atguigu/springmvc/crud/handler/EmployeeHandler.java
@@ -0,0 +1,21 @@
+package com.atguigu.springmvc.crud.handler;
+
+import com.atguigu.springmvc.crud.dao.EmployeeDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class EmployeeHandler {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @RequestMapping("/emps")
+    public String list(Map<String, Object> map){
+        map.put("employees", employeeDao.getAll());
+        return "list";
+    }
+}
--- web/index.jsp
+++ web/index.jsp
@@ -8,9 +8,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>Employess</title>
   </head>
   <body>
-  $END$
+  <a href="emps">List All Employees</a>
   </body>
 </html>
