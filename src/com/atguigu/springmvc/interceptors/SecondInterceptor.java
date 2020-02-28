7320ae03b893f9f792c409d5d70db2ff8ca3eb53
向心力 <>
2020-02-28 20:44:35 +0800

    Interceptors

--- src/com/atguigu/springmvc/interceptors/SecondInterceptor.java
+++ src/com/atguigu/springmvc/interceptors/SecondInterceptor.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class FirstInterceptor implements HandlerInterceptor {
+public class SecondInterceptor implements HandlerInterceptor {
 
     /**
      * 该方法在目标方法之前调用。
@@ -23,7 +23,7 @@ public class FirstInterceptor implements HandlerInterceptor {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("[FirstInterceptor] preHandle");
+        System.out.println("[SecondInterceptor] preHandle");
         return true;
     }
 
@@ -40,7 +40,7 @@ public class FirstInterceptor implements HandlerInterceptor {
      */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("[FirstInterceptor] postHandle");
+        System.out.println("[SecondInterceptor] postHandle");
     }
 
     /**
@@ -54,6 +54,6 @@ public class FirstInterceptor implements HandlerInterceptor {
      */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("[FirstInterceptor] afterCompletion");
+        System.out.println("[SecondInterceptor] afterCompletion");
     }
 }
